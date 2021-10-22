e8f6cf01236a5469de92df28eaf9ccaf7ebef7ad
notcarbide <luis@0k.cx>
2021-10-22 19:04:47 -0400

    sh: build: Simplify build scripts based on recent changes
    Now that compiling with LLVM's IAS is the default and the cross
    compile triplet is set based on arch, these make commands can be
    simplified. Also clean up the the old script, which is now just
    a copy of the rpi4 script.
    
    Signed-off-by: notcarbide <luis@0k.cx>

--- build-rpi.sh
+++ build-rpi.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+export ARCH=arm
+export LLVM=1
 
 KERNEL=kernel
 CONFIG=bcmrpi_defconfig
 JOBS=`grep processor /proc/cpuinfo|wc -l`
 cdate=`date "+%Y-%m-%d"`
 
-make LLVM=1 LLVM_IAS=1 ARCH=arm \
-  CROSS_COMPILE=arm-linux-gnueabihf- $CONFIG
+make $CONFIG
 scripts/config -e LTO_CLANG_FULL
-logsave build.log make -j$JOBS LLVM=1 LLVM_IAS=1 ARCH=arm \
-  CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+
+logsave build.log make -j$JOBS zImage modules dtbs
+
 if [ -e arch/arm/boot/zImage ]; then
   mkdir -p out/boot/overlays
-  make LLVM=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- \
-    INSTALL_MOD_PATH=out modules_install
+  make INSTALL_MOD_PATH=out modules_install
   rm -rf out/lib/modules/*/build
   rm -rf out/lib/modules/*/source
   cp arch/arm/boot/dts/*.dtb out/boot/
