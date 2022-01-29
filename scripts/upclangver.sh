f22d292bd16a5430e4f67586efd189bffa837636
notcarbide <luis@0k.cx>
2022-01-28 21:39:27 -0500

    scripts: Add script to print clang version
    This script is better off in-tree to keep it stored and maintained.
    
    Signed-off-by: notcarbide <luis@0k.cx>

---
+++ scripts/upclangver.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+CLANG_VERSION=`clang --version | head -1`
+FILE_VERSION=`cat $PWD/clangversion.txt`
+
+echo "$CLANG_VERSION"
+
+if [ "$CLANG_VERSION" != "$FILE_VERSION" ]; then
+  echo "$CLANG_VERSION" > clangversion.txt
+  git add clangversion.txt
+  git commit -m "clangversion: $CLANG_VERSION"
+else
+  echo "Current clang set in clangversion is already the current version."
+fi
