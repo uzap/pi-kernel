0ec7b68e421b720fa04d4acf1a92b6ef2e9bd62b
notcarbide <luis@0k.cx>
2022-03-13 03:20:02 -0400

    zap: get-zllvm: Verify signature for zllvm before extracting
    Adds some layers of verification to help ensure the downloaded
    toolchain came from the uZap Maintainer.
    
    By default, will import the uZap Maintainer public key:
      https://cdn.zap.ooo/keys/luis0k_gpg_key.asc
    The above key is verified via fingerprint within the script.
    
    Add a '--no-verify' option to skip the check.
    
    [GPG signed to prove fingerprint is valid.]
    Signed-off-by: notcarbide <luis@0k.cx>

--- zap/get-zllvm.sh
+++ zap/get-zllvm.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
 
-cd ..
-echo "Downloading the latest ZapDragon LLVM release..."
+INSTALL_DIR=..
+FINGERPRINT="6389B3FC107F3C0C24A8E4FCD85C9504501468CB"
+KEY_IMPORTED=`(gpg --quiet --list-keys --keyid-format=long | grep -q $FINGERPRINT) && echo -n "yes"`
 
-wget -O latest.tar.xz https://cdn.zap.ooo/dl/zllvm/x86/latest.tar.xz && \
-echo "Extracting compressed file..." && \
+cd $INSTALL_DIR
+echo "\nDownloading the latest ZapDragon LLVM release..."
+curl --output latest.tar.xz https://cdn.zap.ooo/dl/zllvm/x86/latest.tar.xz
+
+if [ "$1" != "--no-verify" ]; then
+  echo "\nDownloading signature for verification..."
+  curl --output latest.tar.xz.asc https://cdn.zap.ooo/dl/zllvm/x86/latest.tar.xz.asc
+
+  if [ "$KEY_IMPORTED" != "yes" ]; then
+    echo "\nImporting uZap Maintainer public key..."
+    gpg --keyserver pgp.mit.edu --recv-keys $FINGERPRINT
+
+    # Check again to ensure the right key was imported
+    (gpg --list-keys --keyid-format=long | grep -q $FINGERPRINT) || \
+      { echo "\nExpected fingerprint was not found.\nCheck GPG keys immediately!"; exit 1; }
+  fi
+
+  echo "\nVerifying download..."
+  gpg --verify latest.tar.xz.asc latest.tar.xz || \
+    { echo "\nFailed to verify compressed tarball."; exit 1; }
+fi
+
+echo "\nExtracting compressed file..."
 tar -xf latest.tar.xz
 
 echo "ZapDragon LLVM installed in $PWD/zllvm-14.\n"
 echo "Use the following parameter to build using zllvm:"
-echo "  ./compile LLVM_DIR=$PWD/zllvm-14"
+echo "  ./compile LLVM_DIR=$PWD/zllvm-14\n"
 cd - > /dev/null
